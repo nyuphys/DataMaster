249ad29686e3b28c78ef33e4192e58d6db9fc02c
psanker <patricksanker@gmail.com>
2017-05-09 02:08:57 -0400

    Maybe make the circuit physical why don't you

--- example/lab.py
+++ example/lab.py
@@ -8,10 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from scipy.optimize import curve_fit
-import sympy as sp
-
-# Allows LaTeX output in Jupyter and in matplotlib
-sp.init_printing(use_latex=True, use_unicode=True)
 
 #############################################################
 # 2. General Functions
@@ -47,14 +43,12 @@ def lsq(x, y):
 # 3. Data & Globals
 #############################################################
 
-voltage = np.array([0.5, 1.0, 1.5, 2.0, 2.5, 3.0]) # V
 current = np.array([5.372, 10.024, 14.975, 20.482, 24.878, 30.105]) * 1e-3 # mA
+voltage = np.array([0.503, 1.043, 1.526, 2.034, 2.521, 3.018]) # V
 
-# The multimeter tends to have a variable uncertainty, so this array is needed
-dI      = np.array([0.001, 0.002, 0.002, 0.001, 0.001, 0.003])
-
-# Uncertainty in voltage source
-dV      = np.ones(6) * 0.1
+# The multimeter tends to have a variable uncertainty, so these arrays is needed
+dI      = np.array([0.001, 0.002, 0.002, 0.001, 0.001, 0.003]) * 1e-3
+dV      = np.array([0.002, 0.001, 0.003, 0.001, 0.001, 0.002])
 
 #############################################################
 # 4. Lab-Specific Functions
@@ -63,29 +57,23 @@ dV      = np.ones(6) * 0.1
 def plot_line():
 
     # Least-squares linear regression for y = mx + b
-    m, b, sy, sm, sb, r = lsq(voltage, current * 1e3) # We want to plot in mA
+    m, b, sy, sm, sb, r = lsq(current * 1e3, voltage) # We want to plot in mA
 
     # You will NEED to call this for each plot so that you don't have multiple plots
     # overlaid on each other
     plt.figure()
 
     # Range upon which we want to plot the line
-    x = np.linspace(0.1, 3.1, 1000)
+    x = np.linspace(5, 31, 1000)
     plt.plot(x, m*x + b, 'c--')
 
-    plt.errorbar(x=voltage, y=(current * 1e3), xerr=dV, yerr=(dI * 1e3), fmt='r.', ecolor='k', alpha=0.5)
+    plt.errorbar(x=(current * 1e3), y=voltage, xerr=(dI * 1e3), yerr=dV, fmt='r.', ecolor='k', alpha=0.5)
 
-    plt.xlabel('Voltage ($V$)')
-    plt.ylabel('Current ($mA$)')
+    plt.xlabel('Current ($mA$)')
+    plt.ylabel('Voltage ($V$)')
 
 def get_resistance():
-    m, b, sy, sm, sb, r = lsq(voltage, current)
+    m, b, sy, sm, sb, r = lsq(current, voltage)
 
-    # Translate to resistance
-    R  = 1. / m
-
-    # Propagate uncertainty
-    dR = np.sqrt(sm / (m**2.))
-
-    # Array of (resistance, uncertainty)
-    return (R, dR)
+    # Resistance is the slope m; its uncertainty sm is already computed by lsq()
+    return (m, sm)
